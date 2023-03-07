97877d131551d81059315feb4fcfeb9f90fbcd2a
Ajju <abhamre50@gmail.com>
2023-03-07 21:52:43 +0530

    added a  file multiply.java

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static int multiplyNums(int a, int b){
+        return a * b ;
+    }
+    public static void main(String[] args) {
+        System.out.println(multiplyNums(3,3));
+    }
+}
