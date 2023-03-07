1b743bd4f1e54f5267199b8c464397379809906a
Ajju <abhamre50@gmail.com>
2023-03-07 21:13:52 +0530

    initial Commit

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum{
+
+    public static int sum(int x, int y){
+        return x + y; 
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(3,3));
+    }
+}
